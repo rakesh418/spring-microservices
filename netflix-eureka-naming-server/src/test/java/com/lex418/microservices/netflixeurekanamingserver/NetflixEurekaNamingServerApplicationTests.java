dc29fd1eda2a4a3fc4c1dc819c0106dafef868b0
lex418 <rakesh418@gmail.com>
2020-10-06 23:45:04 +0530

    Eureka server integrated

---
+++ netflix-eureka-naming-server/src/test/java/com/lex418/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lex418.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
